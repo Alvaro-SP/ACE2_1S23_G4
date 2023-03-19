f507ce39c2b4abf09c1be4937ce23c61459ced22
Erwin Vásquez <82339728+Erwin14k@users.noreply.github.com>
2023-03-19 12:35:16 -0600

    DB Script update
    DB Script Update

---
+++ Proyecto 1/backend/dbScript.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE Proyecto1;
+
+CREATE TABLE IF NOT EXISTS usuario (
+    idusuario INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(45) NULL,
+    PRIMARY KEY (idusuario)
+);
+
+CREATE TABLE IF NOT EXISTS sesion (
+    idsesion INT NOT NULL AUTO_INCREMENT,
+    ejecucion INT NULL,
+    descanso INT NULL,
+    pomodoro1 INT NULL,
+    pomodoro2 INT NULL,
+    pomodoro3 INT NULL,
+    pomodoro4 INT NULL,
+    descanso1 INT NULL,
+    descanso2 INT NULL,
+    descanso3 INT NULL,
+    descanso4 INT NULL,
+    fecha DATETIME NULL,
+    usuario_idusuario INT NOT NULL,
+    PRIMARY KEY (idsesion),
+    CONSTRAINT fk_sesion_usuario
+        FOREIGN KEY (usuario_idusuario)
+        REFERENCES usuario(idusuario)
+);
