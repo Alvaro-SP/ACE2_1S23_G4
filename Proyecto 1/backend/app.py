1b3b774b34a99f1306a5d5a9a316c4e6dc84786c
Erwin Vásquez <82339728+Erwin14k@users.noreply.github.com>
2023-03-22 13:17:07 -0600

    Session Validations
    Session Validations

--- Proyecto 1/backend/app.py
+++ Proyecto 1/backend/app.py
@@ -41,15 +41,43 @@ def hello():
     global change
     global penalties
     global state
+    global actual_username
     print(request.json)
 
     if request.json["state"] == 0: # Modo setup
         #Verificamos que ahorita mismo hubo un cambio de estado(De Trabajo a setup)
         if state==1:
-            state=0
-            #Ya que validamos que hubo un cambio, indicamos que la sesión acabó, y guardamos los datos.
-
-
+            #Ya que validamos que hubo un cambio, indicamos que la sesión acabó, y guardamos los datos en la db.
+            user_id = get_user_id_by_username(actual_username)
+            # Formando la consulta
+            sql = '''INSERT INTO sesion (ejecucion, descanso, pomodoro1,pomodoro2,pomodoro3,pomodoro4,descanso1,descanso2,descanso3,descanso4,usuario_idusuario) 
+            VALUES (%s, %s, %s)'''
+            values= [(int(work), int(rest), int(penalties[0]),int(penalties[1]),int(penalties[2]),int(penalties[3]),int(penalties[4]),int(penalties[5]),int(penalties[6]),int(penalties[7]),int(user_id))]
+            # Intentando realizar la consulta
+            try:
+                with conecction.cursor() as cursor:
+                    cursor.executemany(sql, values)
+                    conecction.commit()
+                    response = {
+                        "state": "Perfect",
+                        "message": "Session saved Successfully!!"
+                    }
+                    # Resetear el contador de penalizaciones
+                    penalties=[]
+                    for i in range(8):
+                        penalties.append(0)
+                    # Cambiamos el estado a setup
+                    state=0
+                    return response
+            except:
+                traceback.print_exc()
+                print('Error inserting new User')
+                conecction.rollback()
+                response = {
+                    "state": "Error",
+                    "message": "Ha ocurrido un error al insertar la sesión"
+                }
+                return response
         if change:
             change = False
             return jsonify({
@@ -63,7 +91,61 @@ def hello():
                 "type":"no_change"
             })
     else:           
-        
+        #Verificamos que ahorita mismo hubo un cambio de estado(De setup a trabajo)
+        if state==0:
+            state=1
+        # Resetear el contador de penalizaciones
+        penalties=[]
+        for i in range(8):
+            penalties.append(0)
+
+        # Modo trabajo
+        # Verificamos en que fase del pomodoro vamos:
+        phase = request.json['pomodoro']
+        button=request.json['button']
+        # Fase 1 - Pomodoro 1
+        if int(phase)==1:
+            # Debería estar sentado, de lo contrario la penalización suma
+            if int(button)==0:
+                penalties[0]+=1
+        # Fase 2 - Descanso 1
+        if int(phase)==2:
+            # Debería estar parado, de lo contrario la penalización suma
+            if int(button)==1:
+                penalties[1]+=1
+        # Fase 3 - Pomodoro 2
+        if int(phase)==3:
+            # Debería estar sentado, de lo contrario la penalización suma
+            if int(button)==0:
+                penalties[2]+=1
+        # Fase 4 - Descanso 2
+        if int(phase)==4:
+            # Debería estar parado, de lo contrario la penalización suma
+            if int(button)==1:
+                penalties[3]+=1
+        # Fase 5 - Pomodoro 3
+        if int(phase)==5:
+            # Debería estar sentado, de lo contrario la penalización suma
+            if int(button)==0:
+                penalties[4]+=1
+        # Fase 6 - Descanso 3
+        if int(phase)==6:
+            # Debería estar parado, de lo contrario la penalización suma
+            if int(button)==1:
+                penalties[5]+=1
+        # Fase 7 - Pomodoro 4
+        if int(phase)==7:
+            # Debería estar sentado, de lo contrario la penalización suma
+            if int(button)==0:
+                penalties[6]+=1
+        # Fase 8 - Descanso 4
+        if int(phase)==8:
+            # Debería estar parado, de lo contrario la penalización suma
+            if int(button)==1:
+                penalties[7]+=1
+        return jsonify({
+            "type":"work"
+        })
 
 
 @app.route('/datauser',methods=['POST'])
@@ -79,36 +161,6 @@ def data_user():
         "Status": "Data updated Successfully!!"
     }
     return response
-    '''user_id = get_user_id_by_username(user)
-    # Se crea un nuevo usuario en el caso de que no exista
-    if user_id is None:
-        insert_new_user(user)
-        user_id = get_user_id_by_username(user)
-    # Formando la consulta
-    sql = '''INSERT INTO sesion (ejecucion, descanso, usuario_idusuario) 
-            VALUES (%s, %s, %s)'''
-    values= [(int(execution), int(rest), int(user_id))]
-    # Intentando realizar la consulta
-    try:
-        with conecction.cursor() as cursor:
-            cursor.executemany(sql, values)
-            conecction.commit()
-            response = {
-                "state": "Perfect",
-                "message": "Sesión iniciada con éxito!!"
-            }
-            for i in range(8):
-                penalties.append(0)
-            return response
-    except:
-        traceback.print_exc()
-        print('Error inserting new User')
-        conecction.rollback()
-        response = {
-            "state": "Error",
-            "message": "Ha ocurrido un error al insertar la sesión"
-        }
-        return response'''
 
 
 
