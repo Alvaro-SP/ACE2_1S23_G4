818155e21162c9e295e701ecdeed298032d459e8
Alvaro-SP <socop2412@gmail.com>
2023-03-21 19:44:10 -0600

    graph

--- Proyecto 1/frontend/src/components/ChartPercentPomodoro.jsx
+++ Proyecto 1/frontend/src/components/ChartPercentPomodoro.jsx
@@ -3,16 +3,13 @@ import React from "react";
 
 const SVG_WIDTH = 400;
 const SVG_HEIGHT = 300;
-// RECIBE: ["POMODORO", CUMPLIMIENTO,  PENALIZACION_POR_NO_SENTARSE_A_TIEMPO]
+// DEBO SPLITEAR: ["POMODORO", CUMPLIMIENTO,  PENALIZACION_POR_NO_SENTARSE_A_TIEMPO, PENALIZACION_POR_NO_PARARSE]
 const data = [
-    ["lbl1", 100],
-    ["lbl2", 12],
-    ["lbl3", 100],
-    ["etc", 100],
-    ["creo", 100],
-    ["que", 100],
-    ["ta", 12],
-    ["weno", 12],
+    ["Pomodoro 1", 100, 0, 0],
+    ["Pomodoro 2", 20, 0, 80],
+    ["Pomodoro 3", 30, 70, 0],
+    ["Pomodoro 4", 20, 10, 70],
+
 ];
 
 export default function ChartPercentPomodoro() {
@@ -22,10 +19,7 @@ export default function ChartPercentPomodoro() {
     const yAxisLength = SVG_HEIGHT - y0 * 2;
     const xAxisY = y0 + yAxisLength;
     const dataYMax = 100
-    const dataYMin = data.reduce(
-        (currMin, [_, dataY]) => Math.min(currMin, dataY),
-        Infinity
-    );
+    const dataYMin = 0
     const dataYRange = dataYMax - dataYMin;
     const numYTicks = 5;
     const barPlotWidth = xAxisLength / data.length;
@@ -41,42 +35,43 @@ export default function ChartPercentPomodoro() {
 
             {/* X axis */}
             <line
-            x1={x0}
-            y1={xAxisY}
-            x2={x0 + xAxisLength}
-            y2={xAxisY}
-            stroke="grey"
+                x1={x0}
+                y1={xAxisY}
+                x2={x0 + xAxisLength}
+                y2={xAxisY}
+                stroke="grey"
             />
             <text x={x0 + xAxisLength + 5} y={xAxisY + 4}>
-            Tiempo
+                Day
             </text>
 
             {/* Y axis */}
             <line x1={x0} y1={y0} x2={x0} y2={y0 + yAxisLength} stroke="grey" />
             {Array.from({ length: numYTicks }).map((_, index) => {
-            const y = y0 + index * (yAxisLength / numYTicks);
-            const yValue = Math.round(dataYMax - index * (dataYRange / numYTicks));
-            return (
+                const y = y0 + index * (yAxisLength / numYTicks);
+
+                const yValue = Math.round(dataYMax - index * (dataYRange / numYTicks));
+
+                return (
                 <g key={index}>
-                <line x1={x0} y1={y} x2={x0 - 5} y2={y} stroke="grey" />
-                <text x={x0 - 5} y={y + 5} textAnchor="end">
-                {yValue}
-                </text>
+                    <line x1={x0} y1={y} x2={x0 - 5} y2={y} stroke="grey" />
+                    <text x={x0 - 5} y={y + 5} textAnchor="end">
+                    {yValue}
+                    </text>
                 </g>
-            );
+                );
             })}
             
             <text x={x0} y={y0 - 8} textAnchor="middle">%</text>
             {/* Apple plots */}
             {data.map(([day, dataY], index) => {
             const x = x0 + index * barPlotWidth;
-
+            
             const yRatio = (dataY - dataYMin) / dataYRange;
-
             const y = y0 + (1 - yRatio) * yAxisLength;
             const height = yRatio * yAxisLength;
 
-            const applePadding = 2;
+            const applePadding = 5;
 
             // Calculate the radius of the apple based on the height of the bar
             const appleRadius = 15;
@@ -91,19 +86,20 @@ export default function ChartPercentPomodoro() {
                     fill="#866035"
                     title="Porcentaje de Cumplimiento: {}"
                 />
+                
                 {/* Left apple */}
-                <circle
+                {/* <circle
                     cx={x + barPlotWidth / 2}
                     cy={y}
                     r={appleRadius}
                     fill="#F00"
-                />
+                /> */}
 
                 {/* Apple leaf */}
-                <path
+                {/* <path
                     d={`M${x + barPlotWidth / 2},${y - appleRadius + 3} l5,-10 l-20,0 z`}
                     fill="#0B0c"
-                />
+                /> */}
                 <text x={x + barPlotWidth / 2} y={xAxisY + 16} textAnchor="middle">
                     {day}
                 </text>
