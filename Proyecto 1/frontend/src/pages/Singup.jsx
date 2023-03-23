f636a6a08ac4a80c97a6a2be3d5c4385f0a785f9
Alvaro-SP <socop2412@gmail.com>
2023-03-22 23:29:08 -0600

    added the fetch to singup

--- Proyecto 1/frontend/src/pages/Singup.jsx
+++ Proyecto 1/frontend/src/pages/Singup.jsx
@@ -2,13 +2,30 @@ import React from 'react';
 import { TextField, Button, Paper, Typography } from '@mui/material';
 import { Link } from "react-router-dom";
 import addUser from '../img/addUser.png'
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function Singup() {
+   const [usuario, setUsuario] = useState('');
    const handleRegister = (e) => {
       e.preventDefault();
-      alert("Se Registro el usuario")
+      // alert(usuario)
+      // fetch('http://3.15.186.222:5000/register', {
+      axios
+      .post("http://localhost:5000/register", {
+         username: usuario
+      })
+      .then((response) => {
+         console.log(response.data);
+         alert(response.data.Status)
+      })
+      .catch((error) => {
+         console.log(error);
+      });
    }
-
+   const handleChange = (event) => {
+      setUsuario(event.target.value);
+    };
    return (
 
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "70vh" }}>
@@ -18,10 +35,16 @@ export default function Singup() {
             </Typography>
             <img src={addUser} alt="addUser" width={200} style={{marginTop:"2%"}} />
             <Paper elevation={3} style={{ padding: "4%", margin: "5%" }}>
-               
+
                <b>Llena los campos para crear tu usuario:</b>
-              
-               <TextField id="standard-basic" label="Usuario" variant="standard" fullWidth />
+
+               <TextField
+               id="standard-basic"
+               label="Usuario"
+               variant="standard"
+               fullWidth
+               value={usuario}
+               onChange={handleChange} />
                <center>
                   <Button variant="contained"  style={{ marginTop: "4%" }} onClick={handleRegister}>
                      Registrar
