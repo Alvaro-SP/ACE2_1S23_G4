bc7000a97153f847582a464536c0528ac7a8f85f
Alvaro-SP <socop2412@gmail.com>
2023-03-28 21:00:47 -0600

    added name of user in navigation

--- Proyecto 1/frontend/src/components/Navigation.jsx
+++ Proyecto 1/frontend/src/components/Navigation.jsx
@@ -2,9 +2,16 @@ import React, { useState } from 'react';
 import { AppBar, Box, Toolbar, Typography, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import Route from './Route';
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
 export default function Navigation() {
   const [open, setOpen] = useState(false);
-
+  var name="";
+  try {
+    name = cookies.get('usr').name;
+  } catch (error) {
+    name = "no logged";
+  }
   const handleOpenMenu = () => {
     setOpen(true);
   }
@@ -32,6 +39,9 @@ export default function Navigation() {
 
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Pomodoro Portátil - v{process.env.REACT_APP_VERSION}
+            </Typography>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 0 }}>
+              {name}
             </Typography>
 
           </Toolbar>
